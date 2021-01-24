1f2c78607f89ca5eda7dc3814c8eb2e89ae3d0b2
ando-huang <ahuang09182000@gmail.com>
2021-01-24 02:04:48 -0500

    final

--- quantitative_momentum/realistic_momentum.py
+++ quantitative_momentum/realistic_momentum.py
@@ -82,12 +82,86 @@ time_periods = [
 
 for row in hqm_dataframe.index:
     for time_period in time_periods:
-        hqm_dataframe.loc[row, f'{time_period} return Percentile']
-        = stats.percentileofscore(hqm_dataframe[f'{time_period} Price Return'],
+        hqm_dataframe.loc[row, f'{time_period} Return Percentile'] = stats.percentileofscore(hqm_dataframe[f'{time_period} Price Return'],
                 hqm_dataframe.loc[row, f'{time_period} price Return'])/100
 
 from statistics import mean
 
 for row in hqm_dataframe.index:
     momentum_percentiles = []
+    for time_period in time_periods:
+        momentum_percentiles.append(hqm_dataframe.loc[row, f'{time_period} Return Percentile'])
+    hqm_dataframe.loc[row, 'HQM Score'] = mean(momentum_percentiles)
 
+#sort and select best 50
+hqm_dataframe.sort_values(by = 'HQM Score', ascending = False)
+hqm_dataframe = hqm_dataframe[:51]
+
+#calculate how many to buy
+p_size = portfolio_input()
+
+position_size = float(p_size)/len(hqm_dataframe.index)
+for index in range(len(hqm_dataframe['Ticker'])-1):
+    hqm_dataframe.loc[index, 'Number of Shares to Buy'] = math.floor(position_size / hqm_dataframe['Price'][index])
+
+writer = pd.ExcelWriter('momentum_strategy.xlsx', engine='xlsxwriter')
+hqm_dataframe.to_excel(writer, sheet_name='Momentum Strategy', index = False)
+
+background_color = '#0a0a23'
+font_color = '#ffffff'
+
+string_template = writer.book.add_format(
+        {
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+dollar_template = writer.book.add_format(
+        {
+            'num_format':'$0.00',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+integer_template = writer.book.add_format(
+        {
+            'num_format':'0',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+percent_template = writer.book.add_format(
+        {
+            'num_format':'0.0%',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+column_formats = {
+                    'A': ['Ticker', string_template],
+                    'B': ['Price', dollar_template],
+                    'C': ['Number of Shares to Buy', integer_template],
+                    'D': ['One-Year Price Return', percent_template],
+                    'E': ['One-Year Return Percentile', percent_template],
+                    'F': ['Six-Month Price Return', percent_template],
+                    'G': ['Six-Month Return Percentile', percent_template],
+                    'H': ['Three-Month Price Return', percent_template],
+                    'I': ['Three-Month Return Percentile', percent_template],
+                    'J': ['One-Month Price Return', percent_template],
+                    'K': ['One-Month Return Percentile', percent_template],
+                    'L': ['HQM Score', integer_template]
+                    }
+
+for column in column_formats.keys():
+    writer.sheets['Momentum Strategy'].set_column(f'{column}:{column}', 20, column_formats[column][1])
+    writer.sheets['Momentum Strategy'].write(f'{column}1', column_formats[column][0], string_template)
+
+writer.save()
